08538fd7f86e0db507911f289591828bfe7cc42e
Rodrigo Pacheco <rodrigo.rp.pacheco@gmail.com>
2023-01-16 18:30:31 -0300

    First project commit

---
+++ Stocks/Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Rodrigo Pacheco on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
