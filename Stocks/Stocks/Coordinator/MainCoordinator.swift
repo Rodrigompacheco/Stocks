3342b35193c3d68e836a895fb6dbb8d9c7b01170
Rodrigo Pacheco <rodrigo.rp.pacheco@gmail.com>
2023-01-16 21:17:07 -0300

    Created coordinator

---
+++ Stocks/Stocks/Coordinator/MainCoordinator.swift
@@ -0,0 +1,32 @@
+//
+//  MainCoordinator.swift
+//  Stocks
+//
+//  Created by Rodrigo Pacheco on 16/01/23.
+//
+
+import UIKit
+import XCAStocksAPI
+
+class MainCoordinator: Coordinator {
+
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.navigationBar.barStyle = .black
+        navigationController.navigationBar.barTintColor = .red
+        navigationController.navigationBar.tintColor = .white
+    }
+    
+    func start() {
+        let service = XCAStocksAPI()
+        //TODO: Initialize VC
+//        let viewController = ViewController(presenter: Presenter(service: service))
+//        viewController.title = "Stocks"
+//        navigationController.pushViewController(viewController, animated: false)
+    }
+}
